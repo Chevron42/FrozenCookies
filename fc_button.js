f3ffbf05552988e01900b595aaf794c21594a5ce
Andrew Dickinson <35815613+awldickinson@users.noreply.github.com>
2019-10-22 14:22:19 -0400

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -418,7 +418,7 @@ function updateTimers() {
     }
     height = $('#backgroundLeftCanvas').height() - 140;
     // Testing to see if I can turn the circles off.
-    if(FrozenCookies.preferenceValues[drawCircles]) {
+    if(FrozenCookies.preferenceValues[drawCircles] == 1) {
         drawCircles(t_draw, 20, height);
     }
 }
